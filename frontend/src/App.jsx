2d20645c47c10865f2be07b4123279215c95758d
ghouse3105 <mohmadghouse436@gmail.com>
2026-07-11 17:14:53 +0530

    Remove login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./landing_page/Login/login.jsx";
 
 import HomePage from "./landing_page/home/HomePage.jsx";
 import SignUp from "./landing_page/SignUp/SignUp.jsx";
@@ -19,16 +18,15 @@ function App() {
     <div className="container-fluid p-0">
       <Navbar />
 
-     <Routes>
-  <Route path="/" element={<HomePage />} />
-  <Route path="/signup" element={<SignUp />} />
-  <Route path="/login" element={<Login />} />
-  <Route path="/about" element={<AboutPage />} />
-  <Route path="/product" element={<ProductPage />} />
-  <Route path="/pricing" element={<PricingPage />} />
-  <Route path="/support" element={<SupportPage />} />
-  <Route path="*" element={<NotFound />} />
-</Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/pricing" element={<PricingPage />} />
+        <Route path="/support" element={<SupportPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
 
       <Footer />
     </div>
